039e02cbadcff7c652b6ae8bd655ab4c8c68cbda
Pin Sukjaroen <pinsukjaroen@Pins-MacBook-Pro.local>
2022-10-11 01:58:45 +0700

    ready web

--- src/config/api-url.js
+++ src/config/api-url.js
@@ -1,5 +1,5 @@
-export default 'http://192.168.1.39:3500';
+// export default 'http://192.168.1.39:3500';
 // export default 'https://cors-everywhere-me.herokuapp.com/http://unite-app.ap-southeast-1.elasticbeanstalk.com';
-// export default 'https://class-unite.com';
+export default 'https://class-unite.com';
 
 // https://cors-everywhere.herokuapp.com/http://myapi.com/v1/users
